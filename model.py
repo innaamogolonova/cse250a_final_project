66e278eb68162f9e9c53a3023a5f842f7cb6741e
Sean Ko <seanko138@gmail.com>
2025-11-20 08:17:22 -0800

    reading in csv and outlining model functions

--- model.py
+++ model.py
@@ -15,3 +15,25 @@ speed_dating_ds = pd.read_csv(model_path, usecols=columns_needed)
 
 print(speed_dating_ds.head(5)) #check
 
+def e_step():
+    #Estimate P(Z|X, Y)
+    return
+
+
+def m_step():
+    return
+
+def EM_training():
+    #calling e_step and m_step for a certain amount of iterations
+    #OR until the improvement to the CPT is under a certain threshold
+
+    #return final CPTS
+    return 
+
+def predict_second_date():
+    #takes in the CPTS
+    #return the probability of 2nd date
+    return 0
+
+cpts = EM_training()
+predict_second_date()
